0d358663f908f053c767314f180b4bfbdbe6cb9a
kooltony77 <69781520+kooltony77@users.noreply.github.com>
2022-09-30 01:12:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+print("=========================")
+print("WHAT A CAR")
+print("=========================")
+#Design
+
+print("Which cars do you like?:  ")
+print('A. Bentley')
+print("B. Porsche")
+print("C. Camero ")
+favcar = input("Please select option A - C : ")
+print(F" Your fav car is {favcar}. ")
+
+print()
+#Q2 
+q2 = print("Would you like a coupe? ")
+coupe = input("Please enter 'Yes or No':. ")
+#Q3
+print()
+print("What color would you like your car? ")
+color = input("Please enter your color. ")
+
+print()
+#q4 
+print("Which engine would you like to have? ")
+print("A. V6")
+print("B. V8")
+engine = input("Please select option 'A or B: '")
+print()
+  #q5
+print("Do you like to race on the highway")
+race = input("Please enter 'Yes or No': ")
+print()
+print("If 'yes' shame on you :(")
+#q6
+print()
+print("Would you like to own a BMW? ")
+BMW = input("Please enter 'Yes or No': ")
+
+print()
+print("I hate BMW")
+
+print()
+
+print("==================")
+print("~ Summary ~")
