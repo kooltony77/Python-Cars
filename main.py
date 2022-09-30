ed0af182d2dd6b05199e2087e988ec9e7ed680d0
kooltony77 <69781520+kooltony77@users.noreply.github.com>
2022-09-30 03:21:44 +0000

    New Update

--- main.py
+++ main.py
@@ -5,46 +5,69 @@ print("=========================")
 
 #Q1. This will display the question
 print("Which cars do you like?:  ")
+#print Bentley
 print('A. Bentley')
+#prints Porsche
 print("B. Porsche")
+#prints camero
 print("C. Camero ")
+#input for selection of options
 favcar = input("Please select option A - C : ")
+#prints your favorite car is
 print(F" Your fav car is {favcar}. ")
 
+#space
 print()
-#Q2 Will display question
-q2 = print("Would you like a coupe? ")
-coupe = input("Please enter 'Yes or No':. ")
-#Q3
-print()
-print("What color would you like your car? ")
-color = input("Please enter your color. ")
 
+#prints txt
+q2 = print("Would you like a coupe? ")
+#prints yes or no
+coupe = input("Please enter 'Yes or No': ")
+
+#space
 print()
-#q4 
+#prints txt
+print("What color would you like your car? ")
+#prints user input 
+color = input("Please enter your color. ")
+#space
+print()
+
+#prints txt
 print("Which engine would you like to have? ")
+#prints txt
 print("A. V6")
+#prints txt
 print("B. V8")
+#print select options
 engine = input("Please select option 'A or B: '")
 
 print()
-  #q5
+  #prints txt
 print("Do you like to race on the highway")
+#prints user input
 race = input("Please enter 'Yes or No': ")
+
 print()
+
 print("If 'yes' shame on you :(")
-#q6
+
 print()
+#prints txt
 print("Would you like to own a BMW? ")
+#prints user input
 BMW = input("Please enter 'Yes or No': ")
 
 print()
+
 print("I hate BMW")
 
 print()
 
 print("==================")
 print("~ Summary ~")
+print(f"Favorite Car: {favcar}")
+print(f"Car Color: {color}")
 print(f"Model Option Coupe: {coupe}")
 print(f"Engine Option: {engine}")
 print(f"Highway: {race}")
